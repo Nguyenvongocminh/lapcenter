a85a2ec52189b9c6e85e22d1d49851bcdc5d7ef9
Ngoc <minh01294716049@gmail.com>
2021-09-18 23:21:01 +0700

    done navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import logo from '../../assets/imgs/desktop-computer.png';
 import './navbar.scss';
+import {Link} from 'react-router-dom';
 
 const Navbar = (props) => (
     <div className='navbar'>
         <p className='logo-container'>
             <img className='logo' src={logo}/>
         </p>
-        <div className='options'>
-            <a className='option'>
+        <div className='options' >
+            <Link a className='option' to='/'>
                 TRANG CHỦ
-            </a>
-            <a className='option'>
+            </Link>
+            <Link a className='option' to='/introduce'>
                 GIỚI THIỆU
-            </a>
-            <a className='option'>
+            </Link>
+            <Link a className='option' to='/contact'>
                 LIÊN HỆ
-            </a>
-            <a className='option'>
-                PHẢN HỒI
-            </a>
+            </Link>
+            <Link a className='option' to='/login'>
+                ĐĂNG NHẬP
+            </Link>
         </div>
     </div>
 )
