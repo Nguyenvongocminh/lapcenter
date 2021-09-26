a9590b30bfe81eaaae5f547957f8071b4d34ffbc
Ngoc <minh01294716049@gmail.com>
2021-09-26 15:22:52 +0700

    handle pagination and design product detail page

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,27 +2,40 @@ import React from 'react';
 import logo from '../../assets/imgs/desktop-computer.png';
 import './navbar.scss';
 import {Link} from 'react-router-dom';
+import { useHistory } from 'react-router';
 
-const Navbar = (props) => (
-    <div className='navbar'>
-        <p className='logo-container'>
-            <img className='logo' src={logo}/>
+
+const Navbar = (props) => {
+    const history = useHistory();
+  
+    const backToHome = () => {
+      history.push("/");
+    }
+  
+    return (
+      <div className="navbar">
+        <p className="logo-container">
+          <img className="logo" src={logo} onClick={backToHome} />
         </p>
-        <div className='options' >
-            <Link a className='option' to='/'>
-                TRANG CHỦ
-            </Link>
-            <Link a className='option' to='/introduce'>
-                GIỚI THIỆU
-            </Link>
-            <Link a className='option' to='/contact'>
-                LIÊN HỆ
-            </Link>
-            <Link a className='option' to='/login'>
-                ĐĂNG NHẬP
-            </Link>
+        <div className="options">
+          <Link className="option" to="/">
+            TRANG CHỦ
+          </Link>
+          <Link className="option" to="/introduce">
+            GIỚI THIỆU
+          </Link>
+          <Link className="option" to="/contact">
+            LIÊN HỆ
+          </Link>
+          <Link className="option" to="/login">
+            ĐĂNG NHẬP
+          </Link>
         </div>
-    </div>
-)
-
-export default Navbar;
+      </div>
+    );
+  };
+  
+  export default Navbar;
+  
+  
+  
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,48 +3,32 @@ import Navbar from "../../components/navbar/navbar";
 import Card from "../../components/card/card";
 import product from "../../assets/data/product";
 import { useState, useEffect } from "react";
-import { Input, Icon, Segment } from "semantic-ui-react";
+import { Input, Icon, Segment,Pagination } from "semantic-ui-react";
 const axios = require("axios");
 
 function Home() {
   const [data, setData] = useState([]);
-
   const [search, setSearch] = useState("");
   const [price, setPrice] = useState("");
   const [brand, setBrand] = useState("");
-  const [loading, setLoading] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   const fetchData = async (url) => {
-    //start call api
-
     setLoading(true);
-    axios
+    await axios
       .get(url)
-
       .then(function (response) {
-        // handle success
-        console.log("api dung", response.data.products);
+        setPageNumber(1)
         setData(response.data.products);
+        console.log(response.data);
+        setTotalPage(response.data.totalPage)
         setLoading(false);
       })
       .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
+        setLoading(false);
       });
-    // end call api
-    // await setData(product);
-    // fetch('https://lap-center.herokuapp.com/api/product')
-    // .then(response => response.json())
-    // .then(data => {
-    //   console.log('Success:', data);
-    //   setData(data.products)
-    // })
-    // .catch((error) => {
-    //   console.error('Error:', error);
-    // });
   };
 
   useEffect(async () => {
@@ -54,59 +38,72 @@ function Home() {
 
   const onChangeSearch = (e) => {
     setSearch(e.target.value);
-    console.log(e.target.value);
   };
 
   const onSubmitSearch = async () => {
-    let url = `https://lap-center.herokuapp.com/api/product?productName=${search}&productBrand=${brand}&orderByColumn=price&orderByDirection=${price}`;
+    let url = `https://lap-center.herokuapp.com/api/product?productName=${search}&productBrand=${brand}&orderByColumn=price&orderByDirection=${price}`
     await fetchData(url);
   };
 
   const onSearchBrand = async (e) => {
-    await setBrand(e.target.value);
-
-    let url = `https://lap-center.herokuapp.com/api/product?productName=${search}&productBrand=${e.target.value}&orderByColumn=price&orderByDirection=${e.target.value}`;
+    setBrand(e.target.value);
+    let url = ` https://lap-center.herokuapp.com/api/product?productName=${search}&productBrand=${e.target.value}&orderByColumn=price&orderByDirection=${price}`
     await fetchData(url);
   };
 
   const sortPrice = async (e) => {
     setPrice(e.target.value);
-
-    let url = `https://lap-center.herokuapp.com/api/product?productName=${search}&productBrand=${brand}&orderByColumn=price&orderByDirection=${e.target.value}`;
+    let url = `https://lap-center.herokuapp.com/api/product?productName=${search}&productBrand=${brand}&orderByColumn=price&orderByDirection=${e.target.value}`
     await fetchData(url);
   };
-
+  const handlePaginationChange = async (e, { activePage }) => {
+    await setLoading(true);
+    await setPageNumber(activePage);
+    let url = `https://lap-center.herokuapp.com/api/product?productName=${search}&productBrand=${brand}&orderByColumn=price&orderByDirection=${price}&pageSize=12&pageNumber=${activePage}`;
+    await axios
+      .get(url)
+      .then(function (response) {
+        // handle success
+        setData(response.data.products);
+        setTotalPage(response.data.totalPage);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
+    
   return (
     <div className="home-container">
       <Navbar />
-      <div className="menuLeft">
-        <Input
-          icon={
-            <Icon
-              name="search"
-              inverted
-              circular
-              link
-              onClick={onSubmitSearch}
-            />
-          }
-          placeholder="Search..."
-          value={search}
-          onChange={onChangeSearch}
-        />
+      <div className="filter">
+        <div className="search">
+          <Input
+            icon={
+              <Icon
+                name="search"
+                inverted
+                circular
+                link
+                onClick={onSubmitSearch}
+              />
+            }
+            placeholder="Search..."
+            value={search}
+            onChange={onChangeSearch}
+          />
+        </div>
         <div className="selectForm">
-          <p>Hãng</p>
           <select className="selectBox" value={brand} onChange={onSearchBrand}>
-            <option selected value=""></option>
+            <option selected value="">Tất cả</option>
             <option value="Asus">ASUS</option>
             <option value="Dell">DELL</option>
             <option value="Acer">ACER</option>
             <option value="Lenovo">LENOVO</option>
           </select>
         </div>
-
         <div className="selectForm">
-          <p>Giá</p>
           <select className="selectBox" value={price} onChange={sortPrice}>
             <option selected value=""></option>
             <option value="asc">Từ thấp đến cao</option>
@@ -114,13 +111,37 @@ function Home() {
           </select>
         </div>
       </div>
-      <Segment loading={loading} className="product">
-        {data.map((item) => (
-          <Card product={item} />
-        ))}
-        ;
-      </Segment>
-      <div className="menuRight"></div>
+      <div className="container-body">
+        <div className="menuLeft">
+         
+        </div>
+        <Segment loading={loading} className="product">
+          {data.length === 0?
+          <div className="noResults">
+            <h1 style={{textAlign:'center',color:'red'}}>không tìm thấy sản phẩm nào</h1>
+          </div> :
+          data.map((item) => (
+            <Card product={item} />
+          ))
+        }
+          ;
+        </Segment>
+        <div className="menuRight"></div>
+      </div>
+      <div className="paginator">
+      <Pagination
+    boundaryRange={0}
+    // defaultActivePage={1}
+    activePage={pageNumber}
+    ellipsisItem={true}
+    firstItem={true}
+    lastItem={true}
+    siblingRange={1}
+    totalPages={totalPage}
+    onPageChange={handlePaginationChange}
+
+  />
+      </div>
     </div>
   );
 }
